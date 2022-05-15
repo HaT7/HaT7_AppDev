9668ac57a3378c5a4c87d8c483ce6b4c3d2ab180
HaT7 <tuanhagcd19902@fpt.edu.vn>
2022-05-15 10:59:07 +0700

    View Component

---
+++ HaT7FptBook/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using HaT7FptBook.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaT7FptBook.ViewComponents
+{
+    public class UserNameViewComponent: ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimIdentity = (ClaimsIdentity) User.Identity;
+            var claims = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userFromdb = _db.ApplicationUsers.FirstOrDefault(a => a.Id == claims.Value);
+            return View(userFromdb);
+        }
+    }
+}
