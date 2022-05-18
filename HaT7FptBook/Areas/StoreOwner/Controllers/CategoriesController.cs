e00405d3c7ebcdf3c1e43b873c3df32813e8c776
HaT7 <tuanhagcd19902@fpt.edu.vn>
2022-05-18 22:28:42 +0700

    Upload Excel Category Store Owner

--- HaT7FptBook/Areas/StoreOwner/Controllers/CategoriesController.cs
+++ HaT7FptBook/Areas/StoreOwner/Controllers/CategoriesController.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
+using ExcelDataReader;
 using HaT7FptBook.Data;
 using HaT7FptBook.Models;
 using HaT7FptBook.Utility;
 using HaT7FptBook.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HaT7FptBook.Areas.StoreOwner.Controllers
@@ -16,25 +20,28 @@ namespace HaT7FptBook.Areas.StoreOwner.Controllers
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _environment;
         private readonly int _recordsPerPage = 12;
-        public CategoriesController(ApplicationDbContext db)
+
+        public CategoriesController(ApplicationDbContext db, IWebHostEnvironment environment)
         {
             _db = db;
+            _environment = environment;
         }
 
         //======================== INDEX ==========================
         [HttpGet]
         public IActionResult Index(int id = 0, string searchString = "")
         {
-            // var claimIdentity = (ClaimsIdentity) User.Identity;
-            // var claims = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
-            // var storeId = _db.Stores.FirstOrDefault(a => a.StoreOwnerId == claims.Value);
+            var claimIdentity = (ClaimsIdentity) User.Identity;
+            var claims = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var storeId = _db.Stores.FirstOrDefault(a => a.StoreOwnerId == claims.Value);
 
-            // if (storeId == null)
-            // {
-            //     ViewData["Message"] = "Error: Store Id not exist. Let's create your Store first";
-            //     return RedirectToAction("Index", "Stores", new { area = "StoreOwner"});
-            // }
+            if (storeId == null)
+            {
+                ViewData["Message"] = "Error: Store Id not exist. Let's create your Store first";
+                return RedirectToAction("Index", "Stores", new { area = "StoreOwner"});
+            }
             
             try
             {
@@ -63,53 +70,96 @@ namespace HaT7FptBook.Areas.StoreOwner.Controllers
             return View(new List<Category>());
         }
 
-        // //======================== DELETE ==========================
-        // [HttpGet]
-        // public IActionResult Delete(int id)
-        // {
-        //     var category = _db.Categories.Find(id);
-        //     _db.Categories.Remove(category);
-        //     _db.SaveChanges();
-        //     return RedirectToAction(nameof(Index));
-        // }
+         //======================== DELETE ==========================
+         [HttpGet]
+         public IActionResult Delete(int id)
+         {
+             var category = _db.Categories.Find(id);
+             _db.Categories.Remove(category);
+             _db.SaveChanges();
+             return RedirectToAction(nameof(Index));
+         }
 
-        //======================== UPSERT ==========================
-        // [HttpGet]
-        // public IActionResult UpSert(int? id)
-        // {
-        //     var claimIdentity = (ClaimsIdentity) User.Identity;
-        //     var claims = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
-        //
-        //     var storeId = _db.Stores.FirstOrDefault(a => a.StoreOwnerId == claims.Value);
-        //
-        //     if (id == 0 || id == null)
-        //     {
-        //         var categoryCreate = new Category();
-        //         return View(categoryCreate);
-        //     }
-        //
-        //     var category = _db.Categories.Find(id);
-        //     return View(category);
-        // }
-        //
-        // [HttpPost]
-        // public IActionResult UpSert(Category category)
-        // {
-        //     if (!ModelState.IsValid)
-        //     {
-        //         return View(category);
-        //     }
-        //     if (category.Id == 0 || category.Id == null)
-        //     {
-        //         _db.Categories.Add(category);
-        //     }
-        //     else
-        //     {
-        //         _db.Categories.Update(category);
-        //     }
-        //
-        //     _db.SaveChanges();
-        //     return RedirectToAction(nameof(Index));
-        // }
+         //======================== UPSERT ==========================
+         [HttpGet]
+         public IActionResult UpSert(int? id)
+         {
+             var claimIdentity = (ClaimsIdentity) User.Identity;
+             var claims = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+        
+             var storeId = _db.Stores.FirstOrDefault(a => a.StoreOwnerId == claims.Value);
+        
+             if (id == 0 || id == null)
+             {
+                 var categoryCreate = new Category();
+                 return View(categoryCreate);
+             }
+        
+             var category = _db.Categories.Find(id);
+             return View(category);
+         }
+        
+         [HttpPost]
+         public IActionResult UpSert(Category category)
+         {
+             if (!ModelState.IsValid)
+             {
+                 return View(category);
+             }
+             if (category.Id == 0 || category.Id == null)
+             {
+                 _db.Categories.Add(category);
+             }
+             else
+             {
+                 _db.Categories.Update(category);
+             }
+        
+             _db.SaveChanges();
+             return RedirectToAction(nameof(Index));
+         }
+         
+         //==================== UPLOAD EXCEL ========================
+         [HttpPost]
+         public IActionResult UploadExcel(IFormFile file)
+         {
+             if (file != null)
+             {
+                 //create folder
+                 var path = Path.Combine(_environment.WebRootPath, "Uploads");
+                 // check if folder is existed if not then create new one
+                 if (!Directory.Exists(path))
+                 {
+                     Directory.CreateDirectory(path);
+                 }
+
+                 string fileName = Path.GetFileName(file.FileName);;
+                 string filePath = Path.Combine(path, fileName);
+                 // store file
+                 using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                 {
+                     file.CopyTo(stream);
+                 }
+
+                 using var streamFile = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read);
+                 using var reader = ExcelReaderFactory.CreateReader(streamFile);
+                 while (reader.Read())
+                 {
+                     var category = new Category()
+                     {
+                         Name = reader.GetValue(0).ToString(),
+                         Description = reader.GetValue(1).ToString()
+                     };
+
+                     _db.Categories.Add(category);
+                 }
+
+                 _db.SaveChanges();
+                 return RedirectToAction("Index");
+
+             }
+
+             return RedirectToAction("Index");
+         }
     }
 }
